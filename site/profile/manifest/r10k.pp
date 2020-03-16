86ecb132b6681d24529ac64f6f08df7030906ea1
BorysShulga <34127373+BorysShulga@users.noreply.github.com>
2020-03-16 19:42:13 +0100

    profile with classes

---
+++ site/profile/manifest/r10k.pp
@@ -0,0 +1,21 @@
+class profile::r10k {
+  
+  
+  class { 'r10k':
+    remote => 'https://github.com/BorysShulga/pp.git',
+  }
+  
+  
+  class {'r10k::webhook::config':
+    use_mcollective => false,
+    enable_ssl => false,
+  }
+  
+  
+  class {'r10k::webhook':
+    user => 'root',
+    group => 'root',
+  }
+
+
+}
