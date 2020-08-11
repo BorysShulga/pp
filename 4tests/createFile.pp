8fa6c6c35159698bea04b8df77cb6428cd390c0e
BorysShulga <34127373+BorysShulga@users.noreply.github.com>
2020-08-11 16:09:08 +0200

    update frase to see if ush works

--- 4tests/createFile.pp
+++ 4tests/createFile.pp
@@ -2,6 +2,6 @@ file { '/tmp/simpleFile':
                 ensure  => 'present',
                 group   => 'wheel',
                 owner   => 'root',
-                content => 'Hello world, so tipical, or no',
+                content => 'This is a new code, I want to deploy it by simple push',
                 mode    => '0644',
 }
